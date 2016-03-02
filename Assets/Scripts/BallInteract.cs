a94427df01f178f2c6c28fcaf24fe32704af7b1b
Cayo <cayobgcm@gmail.com>
2016-03-02 20:24:58 -0300

    Implementação do Arremesso

--- Assets/Scripts/BallInteract.cs
+++ Assets/Scripts/BallInteract.cs
@@ -6,58 +6,108 @@ public class BallInteract : MonoBehaviour
     [SerializeField]
     private Camera CameraPrimeiraPessoa;//Pega a câmera primeira pessoa do player.
     [SerializeField] private GameObject PivotBola;//Guarda o GameObject do pivot da bola.
+
     private Ray RaioDeInteracao;//Guarda o raio que vai sair da minha camera.
     [SerializeField]
     private float AlcanceDeInteracao;//Distancia no qual é possível interagir com um GameObject.
     private RaycastHit hit;//Guarda os valores do GameObject atingido pelo raio.
-    public bool EstaComBola;//Guarda se o personagem esta com a bola (true) ou não (false).
+
+    [SerializeField]private bool EstaComBola;//Guarda se o personagem esta com a bola (true) ou não (false).
+    private bool PodePegarBola;
+    private float TempoDecorridoLançamento;//Guarda o tempo decorrido após o último lançamento.
 
     GameObject Bola;
+
+    [SerializeField][Range(1,200)]private float EscalaDeForca;
+    private float ForcaInicial;
+
+    [SerializeField]
+    [Range(0, -60)]
+    private float AnguloDoArremesso;
 
     // Use this for initialization
     void Start()
     {
         Bola = GameObject.FindGameObjectWithTag("Bola");
+        ForcaInicial = EscalaDeForca;
     }
 
     // Update is called once per frame
     void Update()
     {
+        TempoDecorridoLançamento += Time.deltaTime;
+
         PegarBola();
-        EstaComBola = VerificaBola();
+        VerificaBola();
+        Arremesso();
+        Passe();
     }
 
-    private bool VerificaBola()
+    private void VerificaBola()//Esta função verifica estados da bola.
     {
-
-        if (Bola.transform.parent != null)
+        if (Bola.transform.parent != null)//Verifica se tem alguém com a bola.
         {
-            return true;
+            EstaComBola = true;
         }
         else
         {
-            return false;
+            EstaComBola = false;
         }
+
+        if (TempoDecorridoLançamento >= 1)//Verifica se o personagem arremessou ou tocou a bola a 1 segundo atrás.
+        {
+            PodePegarBola = true;
+        }
+        else
+        {
+            PodePegarBola = false;
+        }
+
     }
 
-    private void PegarBola()
+    private void PegarBola()//Função responsável por pegar a bola do chão e colocar na mão do player.
     {
         Vector3 Pivot = PivotBola.transform.position;
 
         RaioDeInteracao = CameraPrimeiraPessoa.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
 
-        if (Physics.Raycast(RaioDeInteracao, out hit, AlcanceDeInteracao))
+        if (Physics.Raycast(RaioDeInteracao, out hit, AlcanceDeInteracao))//Aqui verifico se o objeto que eu miro esta dentro do alcance de interação do player.
         {
             
-            if (hit.collider.tag == "Bola" && VerificaBola() == false)
+            if (hit.collider.tag == "Bola" && EstaComBola == false && PodePegarBola)//Aqui verifico se onde eu estou mirando é uma bola e se essa bola esta fora da mão de um player.
             {
                 hit.collider.transform.position = Pivot;
-                hit.collider.GetComponent<Rigidbody>().isKinematic = true;
-                hit.transform.parent = PivotBola.transform;
-                //Instantiate(Bola);
-                //hit.collider.transform.position(PivotBola.transform.position);
-                //Destroy(hit.collider.gameObject);
+                hit.collider.GetComponent<Rigidbody>().isKinematic = true;//Aqui eu ligo o kinematic para a bola não ser afetada pela física, colisões, etc.
+                hit.transform.parent = PivotBola.transform;//Aqui eu faço a bola ser filha do GameObject vazio PivotBola que sinaliza onde a bola fica no player.
+                //hit.transform.localRotation = Quaternion.Euler(0,0,0);
             }
         }
+    }
+
+    private void Arremesso()
+    {
+        if (EstaComBola)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                EscalaDeForca = EscalaDeForca + Time.deltaTime;
+
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                TempoDecorridoLançamento = 0;
+
+                Bola.transform.parent = null;
+                Bola.GetComponent<Rigidbody>().isKinematic = false;
+                Bola.GetComponent<Rigidbody>().velocity = PivotBola.transform.forward * EscalaDeForca;
+                EscalaDeForca = ForcaInicial;
+                Debug.Log(EscalaDeForca);
+            }
+        }  
+    }
+
+    private void Passe()
+    {
+        //TODO Implements.
     }
 }
